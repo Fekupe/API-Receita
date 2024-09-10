65b27d80c4bcb99dc87108f4374a6effdf733c74
Gabriel, Candido <fekupe@outlook.com>
2024-09-10 15:39:17 -0300

    Criando os modelos Category, Recipe, User e Rating

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class User (models.Model):
+    nome = models.CharField(max_length=80, blank=False, null=False, unique=True)
+
+class Category (models.Model):
+    classificacao = models.CharField(max_length=80)
+
+class Recipe (models.Model):
+    titulo = models.CharField(max_length=255, unique=True)
+    descricao = models.TextField(blank=False, null=False)
+    ingredientes = models.TextField(blank=False, null=False)
+    modopreparo = models.TextField(blank=False, null=False)
+    tempopreparo = models.DurationField(blank=False, null=False)
+    categoria = models.CharField(max_length=80)
+    autor = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE)
+    datacriacao = models.DateTimeField(blank=False, null=False)
+    publi_priva = models.BooleanField(null=False)
+
+class Rating (models.Model):
+    receita = models.ForeignKey(Recipe, blank=False, null=False, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User,blank=False, null=False, on_delete=models.CASCADE)
+    nota = models.IntegerField()
+    comentario = models.CharField(max_length=255, blank=True, null=True)
